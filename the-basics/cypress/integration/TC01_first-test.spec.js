f4391975a37e721b93ed46ea0a36278dc2c75ebb
Steve Doggett <steve@orbitalwitness.com>
2020-11-27 14:30:26 +0000

    Initial commit

---
+++ the-basics/cypress/integration/TC01_first-test.spec.js
@@ -0,0 +1,13 @@
+﻿describe('Check the page navigation', () => {
+    it('Verify page title', () => {
+        cy.visit('http://qa.way2automation.com')
+        cy.url().should('include', 'index.php')
+        cy.get('#load_box > #load_form > :nth-child(5) > input')
+            .type('Steve')
+        cy.title().should('contains', 'Welcome')
+        cy.title()
+            .then(($text) => {
+                assert.equal($text, 'Welcome to the Test Site')
+            })
+    })    
+})
