62bf8d20f43dea09175bdfd7f85f09860cc89d12
Steve Doggett <steve@orbitalwitness.com>
2020-11-27 15:42:28 +0000

    Handling elements

--- the-basics/cypress/integration/TC01_first-test.spec.js
+++ the-basics/cypress/integration/TC01_first-test.spec.js
@@ -9,5 +9,16 @@
             .then(($text) => {
                 assert.equal($text, 'Welcome to the Test Site')
             })
+        
+        cy.get('#load_form > fieldset:nth-child(5) > label')
+            .eq(1)
+            .then(label => {
+                cy.log(label.text())
+            })
+
+
+        cy.get('#load_form > fieldset:nth-child(5) > label')
+            .eq(1)
+            .should('have.text', 'Name:')
     })    
 })
